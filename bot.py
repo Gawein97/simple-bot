c804825b2913f16b4a293400d43d08d2ddbd7013
Artem Korostilev <arkoros97@gmail.com>
2019-08-25 22:02:21 +0300

    second one

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import telebot
 import re
+import logging
 from misc import BOT_TOKEN
 
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 bot = telebot.TeleBot(BOT_TOKEN)
 slavic_dict = ['славик', 'вяч']
@@ -9,9 +15,13 @@ slavic_dict = ['славик', 'вяч']
 
 @bot.message_handler(content_types=['text'])
 def send_pidor(message):
+    flag = True
     for word in slavic_dict:
-        if re.search(word, str(message), re.I):
+        if re.search(word, message.text, re.I) and (flag is True):
             bot.send_message(message.chat.id, 'славик - пидор')
+            flag = False
 
 
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    logging.info('Bot started')
+    bot.polling(none_stop=True)
